f452540e65c9ccfb029e762479ab2f94da0e73d8
Kevin <artunduaga00@gmail.com>
2024-03-14 12:27:11 -0500

    Creación de la pagina de inicio de maestros

---
+++ compact_geo_track_web/dist/js/instructores.js
@@ -0,0 +1,27 @@
+const boxes = document.getElementById('boxes');
+const box_1 = document.querySelector('.box_1');
+const box_2 = document.querySelector('.box_2');
+
+
+
+box_1.addEventListener('mouseover', (e) => {
+  box_2.style.scale = (0.9);
+  box_2.classList.add('filter');
+})
+
+
+box_1.addEventListener('mouseout', (e) => {
+  box_2.style.scale = (1);
+  box_2.classList.remove('filter');
+})
+
+box_2.addEventListener('mouseover', (e) => {
+  box_1.style.scale = (0.9);
+  box_1.classList.add('filter');
+})
+
+
+box_2.addEventListener('mouseout', (e) => {
+  box_1.style.scale = (1);
+  box_1.classList.remove('filter');
+})
