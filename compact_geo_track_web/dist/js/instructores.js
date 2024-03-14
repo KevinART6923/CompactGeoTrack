f9492826c20118df7e4a7f184ce5c6bdb1c66eea
Kevin <artunduaga00@gmail.com>
2024-03-14 14:30:12 -0500

    Creación de una de las paginas del historial de mediciones

--- compact_geo_track_web/dist/js/instructores.js
+++ compact_geo_track_web/dist/js/instructores.js
@@ -1,14 +1,17 @@
+/*-----------------------------------*\
+ * #Code Created by: Kevin - 2024
+\*-----------------------------------*/
+
+//==================== Cajas ====================\\
+
 const boxes = document.getElementById('boxes');
 const box_1 = document.querySelector('.box_1');
 const box_2 = document.querySelector('.box_2');
-
-
 
 box_1.addEventListener('mouseover', (e) => {
   box_2.style.scale = (0.9);
   box_2.classList.add('filter');
 })
-
 
 box_1.addEventListener('mouseout', (e) => {
   box_2.style.scale = (1);
@@ -19,7 +22,6 @@ box_2.addEventListener('mouseover', (e) => {
   box_1.style.scale = (0.9);
   box_1.classList.add('filter');
 })
-
 
 box_2.addEventListener('mouseout', (e) => {
   box_1.style.scale = (1);
