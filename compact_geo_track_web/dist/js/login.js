0cd9fcaaf9ee0a0e560c9188e78991b0063ce4f4
Kevin <artunduaga00@gmail.com>
2024-03-19 17:52:48 -0500

    Actualización del login y 1 vez terminado

---
+++ compact_geo_track_web/dist/js/login.js
@@ -0,0 +1,78 @@
+//==================== Input Password ====================\\
+/* Copyright (c) 2024 by Mikael Ainalem (https://codepen.io/ainalem/pen/RwWgexd)*/
+
+const container = document.querySelector(".container");
+const hidden = document.querySelector(".hidden");
+const hiddenInput = document.querySelector(".hidden-input");
+const revealed = document.querySelector(".revealed");
+const revealedInput = document.querySelector(".revealed-input");
+const button = document.querySelector(".button");
+const containerId = document.getElementById('container');
+
+
+const timeline = anime
+  .timeline({
+    duration: 300,
+    easing: "cubicBezier(.4, 0, .2, 1)",
+    autoplay: false,
+  })
+  .add(
+    {
+      targets: document.querySelector(".eye-lid"),
+      d:
+        "M -5,-5 V 37 H 15.6 C 15.6,37 21.35124,23.469343 34.312131,23.469343 47.273022,23.469343 53.4,37 53.4,37 H 77 V -5 Z",
+    },
+    0
+  )
+  .add(
+    {
+      targets: document.querySelector(".eye-lashes"),
+      rotateX: ["180deg", "0deg"],
+    },
+    0
+  );
+hiddenInput.addEventListener("input", () => {
+  if (!container.classList.contains("active")) {
+    revealedInput.value = hiddenInput.value;
+
+  }
+});
+revealedInput.addEventListener("input", () => {
+  if (container.classList.contains("active")) {
+    hiddenInput.value = revealedInput.value;
+  }
+});
+button.addEventListener("click", () => {
+  container.classList.toggle("active");
+  timeline.reverse();
+  timeline.play();
+  if (container.classList.contains("active")) {
+    revealedInput.focus();
+    container.classList.add('border');
+    container.classList.remove('blanco');
+  } else {
+    hiddenInput.focus();
+    container.classList.remove('border');
+    container.classList.add('blanco');
+  }
+});
+timeline.reverse();
+timeline.play();
+
+//==================== Parametros de Inicio ====================\\
+
+function redirectToInstructores() {
+  const username = document.getElementById("username").value;
+  const passwordHidden = document.querySelector(".password-hidden").value;
+  const passwordRevealed = document.querySelector('.password-revealed').value;
+
+  if (username === "artunduaga00@gmail.com" && (passwordHidden === "12345678" || passwordRevealed === '12345678')) {
+    window.location.href = "../dist/instructores.html";
+    return false;
+  } else {
+    alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+    return false;
+  }
+}
+
+
